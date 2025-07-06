020661b0de96de8a58d095fa5ba909f2d447dace
01Mozhgan10 <mozhgantavakoli394@gmail.com>
2025-07-06 09:11:44 +0330

    Update Tamrin1.java

--- Tamrin1.java
+++ Tamrin1.java
@@ -1,7 +1,12 @@
+// برنامه ای بنویسید که "سلام دنیا" را چاپ کند؟
 
+//دسته بندی کلاس ها با دستور پکیج
 package Test1;
+//ایجاد کلاس
 public class Tamrin1 {
+    //متد اصلی که شروع کننده اجرای برنامه است
     public static void main(String[] args){
+        //دستور چاپ
         System.out.print("Hello World");
     }
 }
