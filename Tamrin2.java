1b46224a421e72756c92dca7b418111214e5596a
01Mozhgan10 <mozhgantavakoli394@gmail.com>
2025-07-06 09:29:41 +0330

    Update Tamrin2.java

--- Tamrin2.java
+++ Tamrin2.java
@@ -1,19 +1,26 @@
-//برنامه ای که 2 عدد از کاربر بگیرد و مجموع را به دست آورد؟
+//برنامه ای بنویسید که 2 عدد از کاربر بگیرد و مجموع آن ها را نشان دهد؟
+
+//دسته بندی کلاس ها
 package Test1;
-
+//دستور اولیه ای که بتوانیم از کاربر چیزی دریافت کنیم
 import java.util.Scanner;
-
+//ایجاد کلاس
 public class Tamrin2 {
+    //متد اصلی برای شروع برنامه
     public static void main(String[] args) {
+        //ایجاد یک شی و گرفتن اطلاعات وارد شده توسط کاربر
         Scanner num = new Scanner(System.in);
-
+        //دستور چاپ
         System.out.print("Enter the first number: ");
+        //تعریف متغیر از نوع صحیح و وارد کردن عدد اول توسط کاربر
         int num1 = num.nextInt();
-
+        //دستور چاپ
         System.out.print("Enter the second number: ");
+        //تعریف متغیر از نوع صحیح و وارد کردن عدد دوم توسط کاربر
         int num2 = num.nextInt();
-
+        //تعریف متغیر از نوع صحیح و مجموع 2 عدد
         int sum = num1 + num2;
+        //دستور چاپ حاصل جمع
         System.out.println("Sum of numbers: " + sum);
     }
 }
